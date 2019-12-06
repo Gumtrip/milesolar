8c53163164fdc892ed6eaef2d57bf387b1640ebc
gumtrip <gumtrip@126.com>
2019-12-06 11:20:48 +0800

    parent_id 验证添加了string 条件

--- app/Http/Requests/Admin/Product/ProductCategoryRequest.php
+++ app/Http/Requests/Admin/Product/ProductCategoryRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Admin\Product;
 use App\Http\Requests\Admin\BackendRequest as FormRequest;
 use App\Models\Product\ProductCategory;
 use Request;
+use Illuminate\Validation\Rule;
 class ProductCategoryRequest extends FormRequest
 {
 
@@ -21,7 +22,7 @@ class ProductCategoryRequest extends FormRequest
         ];
         switch ($this->method()){
             case 'POST':$data = array_merge($data,[
-                'parent_id'=>['string|nullable',function($attribute, $value, $fail){
+                'parent_id'=>['string|nullable',Rule::notIn([0]),function($attribute, $value, $fail){
                     if(!is_null($value)){
                         $parentCategory = ProductCategory::withDepth()->find($value);
                         if($parentCategory->depth > 1)return $fail('最多支持3级分类');
@@ -29,7 +30,7 @@ class ProductCategoryRequest extends FormRequest
                 }]
             ]);break;
             case 'PATCH':$data = array_merge($data,[
-                'parent_id'=>['string|nullable',function($attribute, $value, $fail){
+                'parent_id'=>['string|nullable',Rule::notIn([0]),function($attribute, $value, $fail){
                     if(!is_null($value)){
                         $parentCategory = ProductCategory::withDepth()->find($value);
                         //这里需要保证父级dept=0,自身允许有一级子节点
