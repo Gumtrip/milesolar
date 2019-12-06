4b9abd19850cc3c153ecf9441884283b6a2d96d9
gumtrip <gumtrip@126.com>
2019-12-06 11:17:08 +0800

    parent_id 验证添加了string 条件

--- app/Http/Requests/Admin/Product/ProductCategoryRequest.php
+++ app/Http/Requests/Admin/Product/ProductCategoryRequest.php
@@ -21,7 +21,7 @@ class ProductCategoryRequest extends FormRequest
         ];
         switch ($this->method()){
             case 'POST':$data = array_merge($data,[
-                'parent_id'=>[function($attribute, $value, $fail){
+                'parent_id'=>['string',function($attribute, $value, $fail){
                     if(!is_null($value)){
                         $parentCategory = ProductCategory::withDepth()->find($value);
                         if($parentCategory->depth > 1)return $fail('最多支持3级分类');
@@ -29,7 +29,7 @@ class ProductCategoryRequest extends FormRequest
                 }]
             ]);break;
             case 'PATCH':$data = array_merge($data,[
-                'parent_id'=>[function($attribute, $value, $fail){
+                'parent_id'=>['string',function($attribute, $value, $fail){
                     if(!is_null($value)){
                         $parentCategory = ProductCategory::withDepth()->find($value);
                         //这里需要保证父级dept=0,自身允许有一级子节点
