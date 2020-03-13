d67d07a0329694a6fb807cdc5908388fa1dc0d4a
gumtrip <gumtrip@126.com>
2020-03-13 09:54:34 +0800

    上传测试

--- app/Models/Article/Article.php
+++ app/Models/Article/Article.php
@@ -14,8 +14,6 @@ class Article extends Model
     protected $fillable=['title','category_id','image','intro','desc','seo_title','seo_keywords','seo_desc','order'];
     protected $appends=['mid_img','sm_img','create_date'];
 
-
-
     public function category(){
         return $this->belongsTo(ArticleCategory::class);
     }
