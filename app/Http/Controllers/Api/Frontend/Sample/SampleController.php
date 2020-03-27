1af935fada42d916a1d568e21e83e76ff775dae4
gumtrip <gumtrip@126.com>
2020-03-27 20:14:01 +0800

    加入案例模块前台api

--- app/Http/Controllers/Api/Frontend/Sample/SampleController.php
+++ app/Http/Controllers/Api/Frontend/Sample/SampleController.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\Frontend\Sample;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Sample\SampleResource;
+use App\Http\Queries\Sample\SampleQuery;
+use App\Models\Sample\Sample;
+
 
 class SampleController extends Controller
 {
-    //
+    public function index(Request $request, SampleQuery $sample){
+        if($take = $request->take){
+            $samples = $sample->take($take)->get();
+            return new SampleResource($samples);
+        }else{
+            $samples = $sample->paginate();
+        }
+        return SampleResource::collection($samples);
+    }
+
+    public function show(Request $request, Sample $sample){
+        return new SampleResource($sample);
+    }
 }
