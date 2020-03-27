c72f0a8334d3c4d45729db528148fa3d65027b2c
gumtrip <gumtrip@126.com>
2020-03-27 17:30:43 +0800

    完成加入案例模块

--- app/Http/Controllers/Api/Frontend/Article/ArticleController.php
+++ app/Http/Controllers/Api/Frontend/Article/ArticleController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Api\Frontend\Article;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Article\ArticleResource;
-use App\Http\Queries\Article\SampleQuery;
+use App\Http\Queries\Article\ArticleQuery;
 use App\Models\Article\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index(Request $request, SampleQuery $article){
+    public function index(Request $request, ArticleQuery $article){
         if($take = $request->take){
             $articles = $article->take($take)->get();
             return new ArticleResource($articles);
