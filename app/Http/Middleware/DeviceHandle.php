9e1f8787c65c28787613f38f871269724a7e6d06
gumtrip <gumtrip@126.com>
2020-04-09 10:01:33 +0800

    手机版跳转至二级域名

--- app/Http/Middleware/DeviceHandle.php
+++ app/Http/Middleware/DeviceHandle.php
@@ -17,7 +17,7 @@ class DeviceHandle
     {
         $serveName = $request->server('SERVER_NAME');//一级域名
         $requestUri = $request->getRequestUri();//完整地址包括参数
-        $scheme = $request->server('REQUEST_SCHEME');
+        $scheme = 'https';
         if(isMobile()&&!mobileDomain()){//是手机版且 不是二级域名
             return redirect($scheme.'://'.'m.'.$serveName.$requestUri);
         }elseif(!isMobile()&&mobileDomain()){//不是手机版 是二级域名
