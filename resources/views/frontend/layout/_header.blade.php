b99c1e5ae5928e12a3c39b40bd752c20d98c6140
Gumtrip <408156312@qq.com>
2020-10-08 10:50:42 +0800

    切图：头部

--- resources/views/frontend/layout/_header.blade.php
+++ resources/views/frontend/layout/_header.blade.php
@@ -1,6 +1,18 @@
+<div id="headerTop" >
+  <div class="container">
+    <div class="row align-items-center listBox">
+      <div class="col-3 contactItem">
+        <a href="mailto:milly@milesolar.com"><i class="fa fa-envelope-o"></i><span>milly@milesolar.com</span></a>
+      </div>
+      <div class="col-3 contactItem">
+        <a href="tel:008613889943867"><i class="fa fa-phone"></i><span>0086 13889943867</span></a>
+      </div>
+    </div>
+  </div>
+</div>
 <header id="header">
   <div class="container">
-    <div class="row" id="headerRow">
+    <nav id="nav" class="row align-items-center">
       <div class="col-2">
         <div id="logo" class="flexPic">
           <a href="/">
@@ -8,32 +20,15 @@
           </a>
         </div>
       </div>
-      <div class="col-4">
-        <el-form id="searchForm" name="title" action="{{route('products')}}" :inline="true">
-          <el-form-item>
-            <input class="el-input" type="text" name="title" placeholder="SEARCH WHAT YOU WANT!"/>
-          </el-form-item>
-          <el-form-item>
-            <button class="el-button--primary el-button" type="submit">SEARCH</button>
-          </el-form-item>
-        </el-form>
-      </div>
-      <el-col class="col-3 contactItem">
-        <a href="mailto:milly@milesolar.com"><i class="fa fa-envelope-o"></i><span>milly@milesolar.com</span></a>
-      </el-col>
-      <el-col class=" col-3 contactItem">
-        <a href="tel:008613889943867"><i class="fa fa-phone"></i><span>0086 13889943867</span></a>
-      </el-col>
-    </div>
-  </div>
-
-    <nav class="container" id="nav">
-      <div class="row">
-        @foreach($navList as $nav)
-        <div class="col-2 list">
-          <a href="{{$nav['url']}}">{{$nav['title']}}</a>
+      <div class="col-10">
+        <div class="row">
+          @foreach($navList as $nav)
+            <div class="col-2 list">
+              <a href="{{$nav['url']}}">{{$nav['title']}}</a>
+            </div>
+          @endforeach
         </div>
-        @endforeach
       </div>
     </nav>
+  </div>
 </header>
--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -6,24 +6,24 @@
   <title>@yield('seo_title','China robust power inverter - OEM inverters manufacturer')</title>
   <meta name="description" content="@yield('seo_desc','manufacture power inverter, produce robust power inverters')"/>
   <meta name="keywords" content="@yield('seo_keywords','MILESOLAR produce robust & rugged power inverters approved by CE standard,which provide reliable power supply to backup power system & off-grid solar power system')" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{asset('css/vendor/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
   <div id="app">
     @include('frontend.layout._header')
-    <div class="container">
+    <div class="wrapper">
       @yield('content')
     </div>
-    <div class="wrapper">
+    <div class="container">
       @yield('main_content')
     </div>
     @include('frontend.layout._footer')
   </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="{{asset('js/vendor/jquery-3.5.1.slim.min.js')}}"></script>
+<script src="{{asset('js/vendor/popper.min.js')}}"></script>
+<script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
 @stack('before_vue_scripts')
 @stack('after_vue_scripts')
 </html>
